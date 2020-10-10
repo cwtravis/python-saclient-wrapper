70f60b726468ff03cdf7155815f82ad3b90c8952
Cody Travis <cwtravis@gmail.com>
2020-10-10 02:44:20 -0500

    added target directory arg and changed api lkey to be a json fil
    Added --target arg to specify the target directory to scan.
    
    API Key is now provided in a json file.
    
    SAClient is now a class.

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # python-saclientutil
 This project is a Python wrapper around the HCL AppScan on Cloud SAClient utility.
-The SAClient.py script will aid in automation workflows where IBM/HCL do not have out-of-the box plug-ins or integrations.
+The SAClient.py script will aid in automation workflows where HCL does not have out-of-the box plug-ins or integrations.
 
 # Prerequisites: 
   1. Have an HCL AppScan on Cloud account with API Key
-  2. Install the SAClientUtil and add /bin to the path (tested with SAClient Version 8.0.1336)
-  3. Install Python 3 (tested on 3.7.4)
-  4. Have an application to scan and a well configured appscan-config.xml file.
+  2. Install the SAClientUtil and add /bin to the path (tested with SAClient Version 8.0.1387)
+  3. Install Python 3 (tested on 3.9.0)
+  4. Have an application to scan
  
 # Getting Started:
 
-Start with the example script SAST_Automation_Example.py. This script will step through a simple static analysis automation workflow.
-  1. Generate an IRX file using the provided appscan-config.xml file.
-  2. Login to the ASoC service.
-  3. Verify that the provided AppId exists in your ASoC subscription.
+Start with the example script SAST_Automation_Example.py. This script will step through a simple static analysis automation workflow. An example credentials file is available in the example directory.
+  1. Login to the ASoC service.
+  2. Verify that the provided AppId exists in your ASoC subscription.
+  3. Generate an IRX file using the provided appscan-config.xml file.
   4. Submit the IRX file for analysis, using the provided scan name.
   5. Monitor the scan progress and wait for it to complete.
   6. Download the scan summary as a json file and print some info about the scan.
@@ -22,16 +22,32 @@ Start with the example script SAST_Automation_Example.py. This script will step 
 By default, the example will not upload the irx file generated in step 1. It will upload the sample IRX file included in the example directory (demo.irx)
 This file will scan very quickly for demo purposes. In a real use case, delete or comment out the line that uses demo.irx instead of the real IRX file.
 
-The war file, included in the example directory is the Altoro Mutual (altoromutual.war). This is an application specifically designed to be scanned using IBM Security tools. It is publicly available at:  
-<https://github.com/AppSecDev/AltoroJ>
+The war file, included in the example directory is the Altoro Mutual (altoromutual.war). This is an application specifically designed to be scanned using HCL AppScan. It is publicly available at:  
+<https://github.com/hclproducts/AltoroJ>
+
 
 # Run the Example:  
 Print the help and usage information
-`python SAST_Automation_Example.py -h`  
-  
-Run the example workflow  
-`python SAST_Automation_Example.py <apiKeyId> <apiKeySecret> <ASoC AppId> -c example/appscan-config.xml -s SAST_Scan`  
+```python SAST_Automation_Example.py -h 
+usage: SAST_Automation_Example.py [-h] [-c CONFIG] [-s SCAN] [-t TARGET] credentials_file app
 
+positional arguments:
+  credentials_file      Path to file that contains ASoC API Key.
+  app                   ID or Name of the application in ASoC to associate the scan with.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c CONFIG, --config CONFIG
+                        Path to AppScan Config XML File. If the config file resides in the target scan directory, this
+                        option is not required.
+  -s SCAN, --scan SCAN  Scan Name for the ASoC Portal. If not specified, a generic name is used.
+  -t TARGET, --target TARGET
+                        Directory to target for the scan. If not specified, the working directory is used.
+
+File format for credentials file: {"keyid": "<KEYID>", "keysecret": "<KEYSECRET">}```
+
+Run the example workflow  
+`py SAST_Automation_Example.py creds.json -c example/appscan-config.xml -s SAST_Scan -t example`
 
 # Helpful Links:  
 [HCL AppScan on Cloud Help Center](https://help.hcltechsw.com/appscan/ASoC/Welcome.html)  
--- SAST_Automation_Example.py
+++ SAST_Automation_Example.py
@@ -1,117 +1,158 @@
 # Example script to demonstrate static scan automation with
-# HCL AppScan on Cloud and SAClient.py
+# HCL AppScan on Cloud and saclient.py
 # Prerequisites: 
-#	1. Have an IBM ASoC account with API Key
-#	2. Install the SAClientUtil and add /bin to the path (tested with SAClient Version 6.0.1222)
-#	3. Install Python 3, (This was developed and tested on 3.6.5)
-#	4. Have an application to scan and a well configured appscan-config.xml file
+#    1. Have an HCL ASoC account with API Key
+#    2. Install the SAClientUtil and add /bin to the path (tested with SAClient Version 6.0.1222)
+#    3. Install Python 3, (This was developed and tested on 3.9.0)
+#    4. Have an application to scan
+#   5. (Optional) A well configured appscan-config.xml file
 # Author: Cody Travis
 # Email: cody.travis@hcl.com
-# Date: 2020-01-24
+# Date: 2020-10-06
 
 import SAClient
 import sys
 import datetime
 import argparse
 import os
+import json
 
-parser = argparse.ArgumentParser()
-parser.add_argument("keyId", help="ASoC API KeyID to login to ASoC")
-parser.add_argument("keySecret", help="ASoC API Key Secret to login to ASoC")
-parser.add_argument("appId", help="ASoC AppId to associate scan results")
-parser.add_argument("-c", "--config", help="AppScan Config XML File")
-parser.add_argument("-s", "--scan", help="Scan Name for the ASoC Portal")
+def readCredsFile(filePath):
+    try:
+        file = open(filePath, "r")
+        contents = file.read()
+        credDict = json.loads(contents)
+        file.close()
+    except:
+        return none
+    return credDict
+
+epilog = "File format for credentials file: {\"keyid\": \"<KEYID>\", \"keysecret\": \"<KEYSECRET\">}"
+parser = argparse.ArgumentParser(epilog=epilog)
+parser.add_argument("credentials_file", help="Path to file that contains ASoC API Key.")
+parser.add_argument("app", help="ID or Name of the application in ASoC to associate the scan with.")
+parser.add_argument("-c", "--config", help="Path to AppScan Config XML File. If the config file resides in the target scan directory, this option is not required.")
+parser.add_argument("-s", "--scan", help="Scan Name for the ASoC Portal. If not specified, a generic name is used.")
+parser.add_argument("-t", "--target", help="Directory to target for the scan. If not specified, the working directory is used.")
 
 args = parser.parse_args()
 
+api_key = None
+
+print("=== Setup ===")
+print("Processing Args")
+
+#Check if credential file exists
+if(args.credentials_file is not None and os.path.exists(args.credentials_file)==False):
+    print("Credentials file doesn't exist. The file format should look like this:")
+    print("{\"keyid\": \"<KEYID>\", \"keysecret\": \"<KEYSECRET>\"}")
+    sys.exit(1)
+else:
+    #Check if credential file is in correct format.
+    api_key = readCredsFile(args.credentials_file)
+    if api_key:
+        if "keyid" not in api_key.keys() or "keysecret" not in api_key.keys():
+            print("Credential file is badly formatted. Look at the example in github or create on in this format:")
+            print("{\"keyid\": \"<KEYID>\", \"keysecret\": \"<KEYSECRET>\"}")
+        else:
+            print("API Key successfully loaded from credentials file.")
+    else:
+        print("Error readying the credentials file")
+        sys.exit(1)
+    
 if(args.config is not None and os.path.exists(args.config)==False):
-	print("Config file does not exist")
-	sys.exit(1)
+    print("Config file does not exist")
+    sys.exit(1)
 else:
-	configFile = args.config
+    configFile = args.config
 
+if args.target is not None and os.path.exists(args.target)==False and os.path.isdir(args.target)==False:
+    print("Target directory {"+str(args.target)+"} not found.")
+    print("Exiting")
+    sys.exit(1)
+else:
+    targetDir = args.target
+    os.chdir(targetDir)
+    print("Changed working directory to target directory:\n"+str(os.getcwd()))
+    
 if(args.scan is not None):
-	scanName = args.scan
+    scanName = args.scan
 else:
-	scanName = "Static_Scan"
-	
-apiKeyId = args.keyId
-apiKeySecret = args.keySecret
-appId = args.appId
+    scanName = "Static_Scan"
 
-# Step 1: Generate IRX File
-print("=== Step 1: Generate the IRX File ===")
-irxFile = SAClient.generateIRX(scanName, configFile)
+print()
+
+appidentifier = args.app
+debug = True
+saclient = SAClient.SAClient(api_key["keyid"], api_key["keysecret"], False, debug)
+
+# Step 1: Login to ASoC
+print("=== Step 1: Login to ASoC ===")
+if not saclient.loginASoC():
+    print("Could not login to ASoC... bad API key?")
+    print("Exiting")
+    sys.exit(1)
+
+print("Logged in successfully")
+app = saclient.findSingleApp(appidentifier)
+print()
+
+print("=== Step 2: Verify the App Exists ===")
+if not app:
+    print("Could not find app: "+str(appidentifier))
+    print("Check ASoC portal and verify app name or id is correct")
+    print("Exiting")
+    sys.exit(1)
+    
+print("Application Found: Name ["+str(app["name"])+"] Id ["+str(app["id"])+"]")
+print()
+
+# Step 3: Generate IRX File
+print("=== Step 3: Generate the IRX File ===")
+irxFile = saclient.generateIRX(scanName, configFile)
 if(irxFile is None):
-	print("Something went wrong generating IRX File")
-	sys.exit(1)
+    print("Something went wrong generating IRX File")
+    sys.exit(1)
 scanName = irxFile
 irxFile = irxFile + ".irx"
 print("IRX File Generated: " + irxFile)
 print()
 
-# Step 2: Login to the ASoC Service
-print("=== Step 2: Login to the ASoC Service ===")
-if(SAClient.loginASoC(apiKeyId, apiKeySecret)):
-	print("Login Successful")
-else:
-	print("Login was unsuccessful")
-	sys.exit(1)
-print()
-
-# Step 3: Verify Application Id Exists
-print("=== Step 3: Verify Application Id Exists ===")
-appList = SAClient.getAppList()
-if(appId in appList):
-	print("Application Id exists in ASoC")
-	print("App Name: [" + appList[appId] + "] AppId: [" + appId + "]")
-else:
-	print("AppId does not exist or you don't have permission to view it")
-	sys.exit(1)
-print()
-
 # Step 4: Submit IRX File for Analysis
 print("=== Step 4: Submit IRX File to ASoC for Analysis ===")
-scanId = SAClient.queueAnalysis(irxFile, scanName, appId)
+scanId = saclient.queueAnalysis(irxFile, scanName, app["id"])
 if(scanId is None):
-	print("Something went wrong while submitting the IRX File")
-	sys.exit(1)
+    print("Something went wrong while submitting the IRX File")
+    sys.exit(1)
 else:
-	print("Scan Created: " + scanId)
+    print("Scan Created: " + scanId)
 print()
 
 # Step 5: Wait for the scan to complete
 print("=== Step 5: Wait for the scan to complete ===")
 result = "Failed"
-try:
-	result = SAClient.waitForScan(scanId, printStatusEveryMins=5)
-except SAClient.UnauthenticatedException:
-	print("Login token has expired. Logging in again and continue waiting for scan to complete")
-	if(SAClient.loginASoC(apiKeyId, apiKeySecret)):
-		print("Login Successful")
-	else:
-		print("Login was unsuccessful")
-		sys.exit(1)
-	result = SAClient.waitForScan(scanId, printStatusEveryMins=5)
-	
+result = saclient.waitForScan(scanId, printStatusEveryMins=1)
+ 
 if(result != "Ready"):
-	print("Problem waiting for scan to finish: Reason - " + result)
-	sys.exit(1)
-print("Scan is ready")
+    print("Problem waiting for scan to finish: Reason - " + result)
+    saclient.destroyToken()
+    sys.exit(1)
+print("Scan is Complete (status='Ready')")
 print()
 
 # Step 6: Retrieve Scan Results
 print("=== Step 6: Retrieve Scan Results and Save to File ===")
-summary = SAClient.getScanSummary(scanId, scanName+"_result.json")
+summary = saclient.getScanSummary(scanId, scanName+"_result.json")
 if(summary is None):
-	print("Something went wrong getting the scan summary")
-	sys.exit(1)
-	
+    print("Something went wrong getting the scan summary")
+    saclient.destroyToken()
+    sys.exit(1)
+    
 print("Scan Summary:")
 print("  Status: " + summary["LatestExecution"]["Status"] + " (" + summary["LatestExecution"]["ExecutionProgress"] + ")")
 startTime = datetime.datetime.strptime(summary["LatestExecution"]["CreatedAt"], '%Y-%m-%dT%H:%M:%S.%fZ')
 endTime = datetime.datetime.strptime(summary["LatestExecution"]["ScanEndTime"], '%Y-%m-%dT%H:%M:%S.%fZ')
-print("  Scan Duration: " + SAClient.strfdelta(endTime-startTime, "{days}d {hours}h {minutes}m {seconds}s"))
+print("  Scan Duration: " + saclient.strfdelta(endTime-startTime, "{days}d {hours}h {minutes}m {seconds}s"))
 print("  High Issues: " + str(summary["LatestExecution"]["NHighIssues"]))
 print("  Medium Issues: " + str(summary["LatestExecution"]["NMediumIssues"]))
 print("  Low Issues: " + str(summary["LatestExecution"]["NLowIssues"]))
@@ -124,10 +165,11 @@ print()
 
 # Step 7: Download HTML Report
 print("=== Step 7: Download HTML Report ===")
-result = SAClient.getReport(scanId, scanName+"_report.html", "html")
+result = saclient.getReport(scanId, scanName+"_report.html", "html")
 if(result == False):
-	print("Problem downloading report")
-	sys.exit(1)
+    print("Problem downloading report")
+    saclient.destroyToken()
+    sys.exit(1)
 
 print("Report Downloaded and saved to " + scanName+"_report.html")
 print()
